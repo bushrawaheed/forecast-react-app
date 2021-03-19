565352a5000e9254dc8c7305e539b112a7d510c6
BushraWaheed <72761774+bushrawaheed@users.noreply.github.com>
2021-03-19 14:49:24 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
+     <div className="container">
      <h1>Weather App</h1>
-     <footer>This project is coded by <a href="https://www.linkedin.com/feed/" target="_blank">Bushra Waheed</a> and its <a href="https://github.com/bushrawaheed/forecast-react-app" target="_blank">open source on Github</a>
+     <footer>This project was coded by <a href="https://www.linkedin.com/feed/" target="_blank">Bushra Waheed</a> and is <a href="https://github.com/bushrawaheed/forecast-react-app" target="_blank">open-sourced on Github</a>
     </footer>
+    </div>
      </div>
   );
 }
